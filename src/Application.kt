4448f29f1137805942acf2939a24de9bf41e7a41
seungyongC <likerberi97@gmail.com>
2022-06-21 12:51:00 +0900

    started project

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,5 @@
 class Application {
 
-
     fun initGame() {
         println("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)")
         var userInputCarList = readLine()
@@ -8,5 +7,4 @@ class Application {
         println("시도할 횟수를 입력해 주세요")
         var userInputChanceNumber = readLine()!!.toInt()
     }
-
 }
